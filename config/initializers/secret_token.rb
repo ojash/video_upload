2b4fdb0c32754fbe48fb88f308f9c6cde05c16e6
Ojash <ojashdahal@lftechnology.com>
2014-06-05 10:25:02 +0545

    initialization of rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VideoUpload::Application.config.secret_key_base = '9b74a38a1c8ee8187b964c1a979ecb199681ca78d7b7bea75ea65b7732b5a8aa7fe3d1671935b10382d1e5b08057b78b3a3371911716e553ea431821f1a04e83'
